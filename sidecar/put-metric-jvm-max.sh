381549b678872fd5a469c0b5297ae920f7e51dc0
t.tanaka@gx <t.tanaka@gxp.co.jp>
2020-12-16 02:19:05 +0900

    refactor

--- sidecar/put-metric-jvm-max.sh
+++ sidecar/put-metric-jvm-max.sh
@@ -3,9 +3,9 @@
 echo "##### start put-metric-jvm-max.sh #####"
 
 function put_metics_data() {
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxHeapMemoryUsageMax --value $JMX_V6 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxNonHeapMemoryUsageMax --value $JMX_V9 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxMetaspaceMax --value $JMX_V21 --unit Bytes
+    for METRIC_NAME in "${!METRIC_RES_LIST[@]}"; do
+        aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name "${METRIC_NAME}" --value "${METRIC_RES_LIST[${METRIC_NAME}]}" --unit Bytes
+    done
 }
 
 # ECSメタデータエンドポイントのURLを変数にセットする
@@ -22,11 +22,14 @@ SERVICE_NAME=`aws ecs list-tags-for-resource --resource-arn $TASK_ARN | jq '.tag
 TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 JMX_PORT=10048
 JMXCLIENT_PATH=/metric/cmdline-jmxclient-0.10.3.jar
+JMX_CMD="java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT"
 
-# JMXクライアントツールでJVMのメトリクスを取得する
-JMX_V6=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" HeapMemoryUsage 2>&1 | grep max | cut -d' ' -f2`
-JMX_V9=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" NonHeapMemoryUsage 2>&1 | grep max | cut -d' ' -f2`
-JMX_V21=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep max | cut -d' ' -f2`
+# JVMのメトリクスを取得しメトリクス名をキーにした連想配列に詰める
+declare -A METRIC_RES_LIST=(
+    ["JmxHeapMemoryUsageMax"]=`$JMX_CMD "java.lang:type=Memory" HeapMemoryUsage 2>&1 | grep max | cut -d' ' -f2`
+    ["JmxNonHeapMemoryUsageMax"]=`$JMX_CMD "java.lang:type=Memory" NonHeapMemoryUsage 2>&1 | grep max | cut -d' ' -f2`
+    ["JmxMetaspaceMax"]=`$JMX_CMD "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep max | cut -d' ' -f2`
+)
 
 DIMENSIONS_TASK=ServiceName=$SERVICE_NAME,TaskName=$TASK_NAME
 DIMENSIONS_SEARVICE=ServiceName=$SERVICE_NAME
--- sidecar/put-metric-jvm.sh
+++ sidecar/put-metric-jvm.sh
@@ -3,20 +3,9 @@
 echo "##### start put-metric-jvm.sh #####"
 
 function put_metics_data() {
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxGCG1OldCollectionCount --value $JMX_V1 --unit Count
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxGCG1OldCollectionTime --value $JMX_V2 --unit Seconds
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxGCG1YoungCollectionCount --value $JMX_V3 --unit Count
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxGCG1YoungCollectionTime --value $JMX_V4 --unit Seconds
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxHeapMemoryUsageCommitted --value $JMX_V5 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxHeapMemoryUsageUsed --value $JMX_V7 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxNonHeapMemoryUsageCommitted --value $JMX_V8 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxNonHeapMemoryUsageUsed --value $JMX_V10 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxMetaspaceCommitted --value $JMX_V20 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxMetaspaceUsed --value $JMX_V22 --unit Bytes
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxConnectionPoolActiveConnections --value $JMX_V29 --unit Count
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxConnectionPoolIdleConnections --value $JMX_V30 --unit Count
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxConnectionPoolTotalConnections --value $JMX_V31 --unit Count
-    aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name JmxConnectionPoolThreadsAwaitingConnection --value $JMX_V32 --unit Count
+    for METRIC_NAME in "${!METRIC_RES_LIST[@]}"; do
+        aws cloudwatch put-metric-data --dimensions $1 --timestamp $TIMESTAMP --namespace Jmx --metric-name "${METRIC_NAME}" --value "${METRIC_RES_LIST[${METRIC_NAME}]}" --unit "${UNIT_TYPE_LIST[${METRIC_NAME}]}"
+    done
 }
 
 # ECSメタデータエンドポイントのURLを変数にセットする
@@ -33,22 +22,42 @@ SERVICE_NAME=`aws ecs list-tags-for-resource --resource-arn $TASK_ARN | jq '.tag
 TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 JMX_PORT=10048
 JMXCLIENT_PATH=/metric/cmdline-jmxclient-0.10.3.jar
+JMX_CMD="java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT"
 
-# JMXクライアントツールでJVMのメトリクスを取得する
-JMX_V1=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=G1 Old Generation,type=GarbageCollector" CollectionCount 2>&1 | cut -d' ' -f6`
-JMX_V2=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=G1 Old Generation,type=GarbageCollector" CollectionTime 2>&1 | cut -d' ' -f6`
-JMX_V3=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=G1 Young Generation,type=GarbageCollector" CollectionCount 2>&1 | cut -d' ' -f6`
-JMX_V4=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=G1 Young Generation,type=GarbageCollector" CollectionTime 2>&1 | cut -d' ' -f6`
-JMX_V5=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" HeapMemoryUsage  2>&1 | grep committed | cut -d' ' -f2`
-JMX_V7=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" HeapMemoryUsage  2>&1 | grep used | cut -d' ' -f2`
-JMX_V8=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" NonHeapMemoryUsage  2>&1 | grep committed | cut -d' ' -f2`
-JMX_V10=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:type=Memory" NonHeapMemoryUsage  2>&1 | grep used | cut -d' ' -f2`
-JMX_V20=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep committed | cut -d' ' -f2`
-JMX_V22=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep used | cut -d' ' -f2`
-JMX_V29=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "com.zaxxer.hikari:type=Pool (HikariPool-1)" ActiveConnections 2>&1 | cut -d' ' -f6`
-JMX_V30=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "com.zaxxer.hikari:type=Pool (HikariPool-1)" IdleConnections 2>&1 | cut -d' ' -f6`
-JMX_V31=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "com.zaxxer.hikari:type=Pool (HikariPool-1)" TotalConnections 2>&1 | cut -d' ' -f6`
-JMX_V32=`java -jar $JMXCLIENT_PATH - localhost:$JMX_PORT "com.zaxxer.hikari:type=Pool (HikariPool-1)" ThreadsAwaitingConnection 2>&1 | cut -d' ' -f6`
+# JVMのメトリクスを取得しメトリクス名をキーにした連想配列に詰める
+declare -A METRIC_RES_LIST=(
+    ["JmxGCG1OldCollectionCount"]=`$JMX_CMD "java.lang:name=G1 Old Generation,type=GarbageCollector" CollectionCount 2>&1 | cut -d' ' -f6`
+    ["JmxGCG1OldCollectionTime"]=`$JMX_CMD "java.lang:name=G1 Old Generation,type=GarbageCollector" CollectionTime 2>&1 | cut -d' ' -f6`
+    ["JmxGCG1YoungCollectionCount"]=`$JMX_CMD "java.lang:name=G1 Young Generation,type=GarbageCollector" CollectionCount 2>&1 | cut -d' ' -f6`
+    ["JmxGCG1YoungCollectionTime"]=`$JMX_CMD "java.lang:name=G1 Young Generation,type=GarbageCollector" CollectionTime 2>&1 | cut -d' ' -f6`
+    ["JmxHeapMemoryUsageCommitted"]=`$JMX_CMD "java.lang:type=Memory" HeapMemoryUsage  2>&1 | grep committed | cut -d' ' -f2`
+    ["JmxHeapMemoryUsageUsed"]=`$JMX_CMD "java.lang:type=Memory" HeapMemoryUsage  2>&1 | grep used | cut -d' ' -f2`
+    ["JmxNonHeapMemoryUsageCommitted"]=`$JMX_CMD "java.lang:type=Memory" NonHeapMemoryUsage  2>&1 | grep committed | cut -d' ' -f2`
+    ["JmxNonHeapMemoryUsageUsed"]=`$JMX_CMD "java.lang:type=Memory" NonHeapMemoryUsage  2>&1 | grep used | cut -d' ' -f2`
+    ["JmxMetaspaceCommitted"]=`$JMX_CMD "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep committed | cut -d' ' -f2`
+    ["JmxMetaspaceUsed"]=`$JMX_CMD "java.lang:name=Metaspace,type=MemoryPool" Usage 2>&1 | grep used | cut -d' ' -f2`
+    ["JmxConnectionPoolActiveConnections"]=`$JMX_CMD "com.zaxxer.hikari:type=Pool (HikariPool-1)" ActiveConnections 2>&1 | cut -d' ' -f6`
+    ["JmxConnectionPoolIdleConnections"]=`$JMX_CMD "com.zaxxer.hikari:type=Pool (HikariPool-1)" IdleConnections 2>&1 | cut -d' ' -f6`
+    ["JmxConnectionPoolTotalConnections"]=`$JMX_CMD "com.zaxxer.hikari:type=Pool (HikariPool-1)" TotalConnections 2>&1 | cut -d' ' -f6`
+    ["JmxConnectionPoolThreadsAwaitingConnection"]=`$JMX_CMD "com.zaxxer.hikari:type=Pool (HikariPool-1)" ThreadsAwaitingConnection 2>&1 | cut -d' ' -f6`
+)
+# メトリクス名をキーをメトリクスの単位を連想配列に詰める
+declare -A UNIT_TYPE_LIST=(
+    ["JmxGCG1OldCollectionCount"]=Count
+    ["JmxGCG1OldCollectionTime"]=Seconds
+    ["JmxGCG1YoungCollectionCount"]=Count
+    ["JmxGCG1YoungCollectionTime"]=Seconds
+    ["JmxHeapMemoryUsageCommitted"]=Bytes
+    ["JmxHeapMemoryUsageUsed"]=Bytes
+    ["JmxNonHeapMemoryUsageCommitted"]=Bytes
+    ["JmxNonHeapMemoryUsageUsed"]=Bytes
+    ["JmxMetaspaceCommitted"]=Bytes
+    ["JmxMetaspaceUsed"]=Bytes
+    ["JmxConnectionPoolActiveConnections"]=Count
+    ["JmxConnectionPoolIdleConnections"]=Count
+    ["JmxConnectionPoolTotalConnections"]=Count
+    ["JmxConnectionPoolThreadsAwaitingConnection"]=Count
+)
 
 DIMENSIONS_TASK=ServiceName=$SERVICE_NAME,TaskName=$TASK_NAME
 DIMENSIONS_SEARVICE=ServiceName=$SERVICE_NAME
